34a521560c662d363a6417f71406906545c536d9
Aarya <aarya.codes11@gmail.com>
2026-08-01 23:56:26 +0530

    Built movie search app with OMDb API integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function Movie(props) {
@@ -13,10 +13,34 @@ function Movie(props) {
 
 function App() {
   const [search, setSearch] = useState("");
+  const [count, setCount] = useState(0);
+  const [movieData, setMovieData] = useState(null);
+
+  const movies = [
+    {title: "Batman", year:2008},
+    {title: "Avengers", year:2012},
+    {title: "Iron Man", year:2008},
+  ];
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(search.toLowerCase())
+);
+
+useEffect(() => {
+  if (search.trim() === "") return;
+
+  fetch(`https://www.omdbapi.com/?apikey=eccad948&t=${search}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setMovieData(data);
+    });
+}, [search]);
 
   return (
     <div>
       <h1>Movie Search</h1>
+      <h2>Count: {count}</h2>
 
       <input
       type="text"
@@ -25,13 +49,37 @@ function App() {
       onChange={(e) => setSearch(e.target.value)}
       />
 
-      <button>Search</button>
+      <button onClick={() => setCount(count +1)}>
+        Increase Count
+      </button> 
 
-      <Movie title="Batman" year={2008} />
-      <Movie title="Avengers" year={2012} />
-      <Movie title="Iron Man" year={2008} />
+      {movieData && movieData.Response === "True" && (
+  <div>
+    <h2>{movieData.Title}</h2>
+    <img
+      src={movieData.Poster}
+      alt={movieData.Title}
+      width="200"
+    />
+    <p>Year: {movieData.Year}</p>
+    <p>IMDb Rating: {movieData.imdbRating}</p>
+  </div>
+)}
+
+     {filteredMovies.length > 0 ? (
+     filteredMovies.map((movie) => (
+          <Movie
+          key={movie.title}
+           title={movie.title}
+            year={movie.year} 
+            />
+     ))
+    ) : (
+      <p>No movies found.</p>
+    )}
      </div>
   );
 }
+
 
 export default App;
